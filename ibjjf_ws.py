ef78e5cc358960b95ff77673b2e1700fb370023b
Kyle Yoneshige <yoneshige.kyle@gmail.com>
2023-03-27 20:53:31 -0700

    Commit W/ Name Parser
    Parser is only pulling last athlete from list.
    
    TO DO: Complete name parser

--- ibjjf_ws.py
+++ ibjjf_ws.py
@@ -7,20 +7,51 @@ URL = "https://www.ibjjfdb.com/ChampionshipResults/1877/PublicResults"
 r = requests.get(URL)
    
 soup = BeautifulSoup(r.content, 'html5lib')
-table = soup.find('div', attrs = {'id':'content'}) 
-# print(table)
+table = soup.find('div', attrs = {'class':"col-xs-12 col-md-6 col-athlete"}) 
 
 athletes = []
 
+#for each division break down table to age, sex, rank, weight
 for row in table.findAll('h4', attrs = {'class': 'subtitle'}):
     athlete={}
-    athlete['division'] = row
-    print(row)
+    #strip out white space
+    noSpace = row.text.strip()
+    #split data at each slash
+    processed = noSpace.split('/')
+
+    athlete['age'] = processed[0].strip()
+    athlete['sex'] = processed[1].strip()
+    athlete['rank'] = processed[2].strip()
+    athlete['weight'] = processed[3].strip()
+
+    #TO DO: Write code to parse athlete name/placement text
+    #Right now code is only pulling last name from list
+    for section in table.findAll('div', attrs = {'class': 'athlete-item'}):
+        nextNode = section
+        name = section.div.text.strip()
+        nameOne = name.strip().split('\n')[0]
+        nameTwo = section.p.text.strip().split("\n")[0]
+        nameThree = section.span.text.strip().split("\n")[0]
+        athlete['name'] = nameOne
+        athlete['team'] = nameTwo
+        athlete['place'] = nameThree
+        while True:
+            nextNode = nextNode.nextSibling
+            try:
+                tag_name = nextNode.name
+            except AttributeError:
+                tag_name = ""
+            if tag_name == "subtitle":
+                print(nextNode.text)
+            break
+    
     athletes.append(athlete)
 
-#print(athletes)
+#To Do: write function for text parser
 
-# with open("results.csv", 'w', newline = '') as file:
-#     writer = csv.writer(file)
-#     for i in table:
-#         writer.writerow(i)
+#write athlete data to csv
+with open("results.csv", 'w', newline = '') as file:
+    writer = csv.DictWriter(file, delimiter=",", fieldnames=['name','team','place','age','sex','rank', 'weight'])
+    writer.writeheader()
+    for athlete in athletes:
+        writer.writerow(athlete)
